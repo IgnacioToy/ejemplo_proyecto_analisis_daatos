4930b403fcde9b34e493ffd5f75c99112870932f
D.L.F <ignacio.n.toyos@gmail.com>
2026-04-17 10:45:51 -0300

    Termine el entrenamiento_modelo.py

--- ejjjjj.py
+++ ejjjjj.py
@@ -1,13 +1,11 @@
 import os
 import sys
 from dataclasses import dataclass
-
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (AdaBoostRegressor, GradientBoostingRegressor, RandomForestRegressor)
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
-
 from src.exception import ExcepcionPersonalizada
 from src.logger import logging
 from src.utils import guardar_objeto, evaluar_modelos
@@ -39,28 +37,26 @@ class EntrenadorModelo:
 
             params = {
                 "Decision Tree": {"criterion": ["squared_error", "friedman_mse"]},
-                "Random Forest": {"n_estimators": [8, 16, 32, 64, 128, 256]},
-                "Gradient Boosting": {"learning_rate": [0.1, 0.01], "subsample": [0.6, 0.7, 0.8]},
+                "Random Forest": {"n_estimators": [32, 64, 128]},
+                "Gradient Boosting": {"learning_rate": [0.1, 0.01], "subsample": [0.6, 0.8]},
                 "Linear Regression": {},
                 "XGBRegressor": {"learning_rate": [0.1, 0.01], "n_estimators": [100, 200]},
-                "CatBoosting Regressor": {"depth": [6, 8, 10], "learning_rate": [0.01, 0.05]},
+                "CatBoosting Regressor": {"depth": [6, 10], "learning_rate": [0.01, 0.05]},
                 "AdaBoost Regressor": {"learning_rate": [0.1, 0.01], "n_estimators": [50, 100]}
             }
 
-            reporte_modelos: dict = evaluar_modelos(X_train, y_train, X_test, y_test, modelos, params)
+            reporte_modelos = evaluar_modelos(X_train, y_train, X_test, y_test, modelos, params)
 
             mejor_puntaje = max(sorted(reporte_modelos.values()))
             mejor_nombre_modelo = list(reporte_modelos.keys())[list(reporte_modelos.values()).index(mejor_puntaje)]
             mejor_modelo = modelos[mejor_nombre_modelo]
 
             if mejor_puntaje < 0.6:
-                raise ExcepcionPersonalizada("No se encontró un modelo con rendimiento aceptable")
+                raise ExcepcionPersonalizada("Ningún modelo superó el umbral del 60%")
 
-            logging.info(f"Mejor modelo encontrado: {mejor_nombre_modelo} con R2 de {mejor_puntaje}")
-
+            logging.info(f"Mejor modelo: {mejor_nombre_modelo} con R2: {mejor_puntaje}")
             guardar_objeto(self.config_entrenador_modelo.ruta_modelo_entrenado, mejor_modelo)
 
             return mejor_puntaje
-
         except Exception as e:
             raise ExcepcionPersonalizada(e, sys)
